ab7084059ab2d2ba8a5b5e10274474192f7034a4
mkamakura <kamakura.masaya@gmail.com>
2014-12-02 21:34:08 +0900

    make errors

--- type.go
+++ type.go
@@ -37,7 +37,7 @@ func NewDocument(url string) (*Document, error) {
 	}
 
 	if res.StatusCode != 200 {
-		e := errors.New(res.statusCode)
+		e := errors.New(res.StatusCode)
 		return nil, e
 	}
 	return NewDocumentFromResponse(res)
